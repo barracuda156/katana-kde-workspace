94c44c9b515dd384aee116d7a6ef9cc7e6e9168e
Ivailo Monev <xakepa10@gmail.com>
2024-04-01 14:26:58 +0300

    plasma: reimplement system-monitor applet
    what is wrong with the old applet implementation? it uses two
    dataengines which by itself is a huge performance penalty. the current
    applet implementation is minimal - shows only the system CPU load and
    that is about it but I intend to expand upon that
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- plasma/applets/system-monitor/system-monitor.cpp
+++ plasma/applets/system-monitor/system-monitor.cpp
@@ -1,296 +1,233 @@
-/*
- *   Copyright (C) 2007 Petri Damsten <damu@iki.fi>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the
- *   Free Software Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+/* 
+    This file is part of the KDE project
+    Copyright (C) 2024 Ivailo Monev <xakepa10@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2, as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
 
 #include "system-monitor.h"
-#include "monitorbutton.h"
-#include "applet.h"
+#include "ksysguard/ksgrd/SensorClient.h"
+#include "ksysguard/ksgrd/SensorManager.h"
+
+#include <QTimer>
 #include <QGraphicsLinearLayout>
+#include <Plasma/Theme>
+#include <Plasma/SignalPlotter>
 #include <KDebug>
-#include <KPushButton>
-#include <Plasma/Containment>
-#include <Plasma/Corona>
-#include <Plasma/ToolTipManager>
+
+typedef QList<QByteArray> SystemMonitorSensors;
+
+static const int s_monitorsid = -1;
+static const char* const s_systemuptimesensor = "system/uptime";
+static const char* const s_cpusystemloadsensor = "cpu/system/TotalLoad";
+static const int s_updatetimeout = 1000;
+
+static QColor kCPUVisualizerColor()
+{
+    return Plasma::Theme::defaultTheme()->color(Plasma::Theme::TextColor);
+}
+
+class SystemMonitorClient : public QObject, public KSGRD::SensorClient
+{
+    Q_OBJECT
+public:
+    SystemMonitorClient(QObject *parent);
+    ~SystemMonitorClient();
+
+    SystemMonitorSensors sensors() const;
+    void requestValue(const QByteArray &sensor) const;
+
+Q_SIGNALS:
+    void sensorValue(const QByteArray &sensor, const float value);
+
+private Q_SLOTS:
+    void slotUpdate();
+
+protected:
+    void answerReceived(int id, const QList<QByteArray> &answer) final;
+    void sensorLost(int id) final;
+
+private:
+    SystemMonitorSensors m_sensors;
+};
+
+SystemMonitorClient::SystemMonitorClient(QObject *parent)
+    : QObject(parent)
+{
+    KSGRD::SensorMgr = new KSGRD::SensorManager(this);
+    KSGRD::SensorMgr->engage("localhost", "", "ksysguardd");
+
+    connect(KSGRD::SensorMgr, SIGNAL(update()), this, SLOT(slotUpdate()));
+    slotUpdate();
+}
+
+SystemMonitorClient::~SystemMonitorClient()
+{
+}
+
+SystemMonitorSensors SystemMonitorClient::sensors() const
+{
+    return m_sensors;
+}
+
+void SystemMonitorClient::requestValue(const QByteArray &sensor) const
+{
+    const int sensorid = m_sensors.indexOf(sensor);
+    if (sensorid < 0) {
+        // this can actually happen if there was no answer yet for "monitors"
+        kWarning() << "unmapped sensor" << sensor;
+        return;
+    }
+    const QString sensorname = QString::fromLatin1(sensor.constData(), sensor.size());
+    KSGRD::SensorMgr->sendRequest("localhost", sensorname, (KSGRD::SensorClient*)this, sensorid);
+}
+
+void SystemMonitorClient::slotUpdate()
+{
+    KSGRD::SensorMgr->sendRequest("localhost", "monitors", (KSGRD::SensorClient*)this, s_monitorsid);
+}
+
+void SystemMonitorClient::answerReceived(int id, const QList<QByteArray> &answer)
+{
+    if (id == s_monitorsid) {
+        foreach (const QByteArray &sensoranswer, answer) {
+            const QList<QByteArray> splitsensoranswer = sensoranswer.split('\t');
+            if (splitsensoranswer.size() != 2) {
+                kWarning() << "invalid sensor answer" << sensoranswer;
+                continue;
+            }
+            const QByteArray sensortype = splitsensoranswer.at(1);
+            if (sensortype != "integer" && sensortype != "float") {
+                continue;
+            }
+            const QByteArray sensorname = splitsensoranswer.at(0);
+            kDebug() << "mapping sensor" << sensorname << sensortype;
+            m_sensors.append(sensorname);
+        }
+    } else if (id < m_sensors.size()) {
+        foreach (const QByteArray &sensoranswer, answer) {
+            const QByteArray sensorname = m_sensors.at(id);
+            const float sensorvalue = sensoranswer.toFloat();
+            kDebug() << "got sensor value" << id << sensorname << sensorvalue;
+            emit sensorValue(sensorname, sensorvalue);
+        }
+    } else {
+        kWarning() << "invalid sensor ID" << id;
+    }
+        
+}
+
+void SystemMonitorClient::sensorLost(int id)
+{
+    kDebug() << "sensor lost" << id;
+    slotUpdate();
+}
+
+
+class SystemMonitorWidget : public QGraphicsWidget
+{
+    Q_OBJECT
+public:
+    SystemMonitorWidget(SystemMonitor* systemmonitor);
+    ~SystemMonitorWidget();
+
+private Q_SLOTS:
+    void slotRequestValues();
+    void slotSensorValue(const QByteArray &sensor, const float value);
+
+private:
+    SystemMonitor* m_systemmonitor;
+    QGraphicsLinearLayout* m_layout;
+    SystemMonitorClient* m_systemmonitorclient;
+    Plasma::SignalPlotter* m_cpuplotter;
+};
+
+
+SystemMonitorWidget::SystemMonitorWidget(SystemMonitor* systemmonitor)
+    : QGraphicsWidget(systemmonitor),
+    m_systemmonitor(systemmonitor),
+    m_layout(nullptr)
+{
+    m_layout = new QGraphicsLinearLayout(Qt::Vertical, this);
+    m_systemmonitorclient = new SystemMonitorClient(this);
+    connect(
+        m_systemmonitorclient, SIGNAL(sensorValue(QByteArray,float)),
+        this, SLOT(slotSensorValue(QByteArray,float))
+    );
+    m_cpuplotter = new Plasma::SignalPlotter(this);
+    m_cpuplotter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    m_cpuplotter->setTitle(i18n("CPU"));
+    m_cpuplotter->setUnit("%");
+    m_cpuplotter->setShowTopBar(false);
+    m_cpuplotter->setShowLabels(true);
+    m_cpuplotter->setShowVerticalLines(false);
+    m_cpuplotter->setShowHorizontalLines(false);
+    m_cpuplotter->setThinFrame(false);
+    m_cpuplotter->setUseAutoRange(false);
+    m_cpuplotter->setVerticalRange(0.0, 100.0);
+    m_cpuplotter->addPlot(kCPUVisualizerColor());
+    m_layout->addItem(m_cpuplotter);
+    setLayout(m_layout);
+
+    slotRequestValues();
+}
+
+SystemMonitorWidget::~SystemMonitorWidget()
+{
+}
+
+void SystemMonitorWidget::slotRequestValues()
+{
+    m_systemmonitorclient->requestValue(s_systemuptimesensor);
+    m_systemmonitorclient->requestValue(s_cpusystemloadsensor);
+    QTimer::singleShot(s_updatetimeout, this, SLOT(slotRequestValues()));
+}
+
+void SystemMonitorWidget::slotSensorValue(const QByteArray &sensor, const float value)
+{
+    if (sensor == s_cpusystemloadsensor) {
+        m_cpuplotter->addSample(QList<double>() << double(value));
+    }
+}
+
 
 SystemMonitor::SystemMonitor(QObject *parent, const QVariantList &args)
-    : Plasma::PopupApplet(parent, args), m_layout(0), m_buttons(0), m_widget(0)
+    : Plasma::PopupApplet(parent, args),
+    m_systemmonitorwidget(nullptr)
 {
+    KGlobal::locale()->insertCatalog("plasma_applet_system-monitor");
     setAspectRatioMode(Plasma::IgnoreAspectRatio);
+    m_systemmonitorwidget = new SystemMonitorWidget(this);
+    setPopupIcon("utilities-system-monitor");
 }
 
 SystemMonitor::~SystemMonitor()
 {
-}
-
-void SystemMonitor::saveState(KConfigGroup &group) const
-{
-    QStringList appletNames;
-    foreach (SM::Applet *applet, m_applets) {
-        applet->saveConfig(group);
-        appletNames << applet->objectName();
-    }
-
-    group.writeEntry("applets", appletNames);
-}
-
-void SystemMonitor::createConfigurationInterface(KConfigDialog *parent)
-{
-    foreach (Plasma::Applet *applet, m_applets) {
-        applet->createConfigurationInterface(parent);
-    }
+    delete m_systemmonitorwidget;
 }
 
 void SystemMonitor::init()
 {
-    KConfigGroup cg = config();
-    QStringList appletNames = cg.readEntry("applets", QStringList());
-
-    m_widget = new QGraphicsWidget(this);
-    m_layout = new QGraphicsLinearLayout(Qt::Vertical);
-    m_layout->setContentsMargins(0, 0, 0, 0);
-    m_buttons = new QGraphicsLinearLayout(Qt::Horizontal);
-    m_buttons->setContentsMargins(0, 0, 0, 0);
-    m_buttons->setSpacing(5);
-
-    QMap<QString, KPluginInfo> appletsFound;
-    KPluginInfo::List appletList = listAppletInfo("System Information");
-    foreach (const KPluginInfo &pluginInfo, appletList) {
-        if (pluginInfo.pluginName().startsWith("sm_") && !pluginInfo.isHidden()) {
-            appletsFound.insert(pluginInfo.pluginName(), pluginInfo);
-        }
-    }
-
-    foreach (const KPluginInfo &pluginInfo, appletsFound) {
-        MonitorButton *button = new MonitorButton(m_widget);
-        button->setObjectName(pluginInfo.pluginName());
-        Plasma::ToolTipContent data;
-        data.setMainText(pluginInfo.name());
-        data.setImage(KIcon(pluginInfo.icon()).pixmap(IconSize(KIconLoader::Desktop)));
-        Plasma::ToolTipManager::self()->setContent(button, data);
-        button->setCheckable(true);
-        button->setImage(pluginInfo.icon());
-        if (appletNames.contains(pluginInfo.pluginName())) {
-            button->setChecked(true);
-        }
-        connect(button, SIGNAL(toggled(bool)), this, SLOT(toggled(bool)));
-        m_buttons->addItem(button);
-        m_monitorButtons << button;
-        // this does not work
-        KGlobal::locale()->insertCatalog(pluginInfo.pluginName());
-    }
-
-    m_layout->addItem(m_buttons);
-    foreach (const QString& appletName, appletNames) {
-        if (appletsFound.contains(appletName)) {
-            Applet * applet = addApplet(appletName);
-
-            if (applet) {
-                Plasma::Constraints constraints(Plasma::ImmutableConstraint |
-                                                Plasma::StartupCompletedConstraint);
-                applet->updateConstraints(constraints);
-                applet->flushPendingConstraintsEvents();
-            }
-        }
-    }
-
-    m_widget->setLayout(m_layout);
-    checkGeometry();
-
-    setPopupIcon("utilities-system-monitor");
-}
-
-void SystemMonitor::toggled(bool toggled)
-{
-    removeApplet(sender()->objectName());
-
-    if (toggled) {
-        SM::Applet * applet = addApplet(sender()->objectName());
-
-        if (applet) {
-            Plasma::Constraints constraints(Plasma::ImmutableConstraint |
-                                            Plasma::StartupCompletedConstraint);
-            applet->updateConstraints(constraints);
-            applet->flushPendingConstraintsEvents();
-        }
-    }
-}
-
-void SystemMonitor::configChanged()
-{
-    KConfigGroup cg = config();
-    QStringList appletNames = cg.readEntry("applets", QStringList());
-
-    QStringList oldAppletNames;
-    foreach (SM::Applet *applet, m_applets) {
-        oldAppletNames << applet->objectName();
-    }
-
-    if (appletNames == oldAppletNames) {
-        foreach (SM::Applet *applet, m_applets)
-            applet->configChanged();
-    } else {
-        QMap<QString, KPluginInfo> appletsFound;
-        KPluginInfo::List appletList = listAppletInfo("System Information");
-        foreach (const KPluginInfo &pluginInfo, appletList) {
-            if (pluginInfo.pluginName().startsWith("sm_") && !pluginInfo.isHidden()) {
-                appletsFound.insert(pluginInfo.pluginName(), pluginInfo);
-            }
-        }
-
-        foreach (MonitorButton *button, m_monitorButtons) {
-            button->setChecked(false);
-        }
-
-        foreach (const QString& appletName, appletNames) {
-            if (appletsFound.contains(appletName)) {
-                foreach (MonitorButton* button, m_monitorButtons) {
-                    if (button->objectName() == appletName)
-                        button->setChecked(true);
-                }
-            }
-        }
-        checkGeometry();
-    }
-}
-
-SM::Applet *SystemMonitor::addApplet(const QString &name)
-{
-    if (name.isEmpty()) {
-        return 0;
-    }
-
-    Plasma::Applet* plasmaApplet = Plasma::Applet::load(name, 0, QVariantList() << "SM");
-    SM::Applet* applet = qobject_cast<SM::Applet*>(plasmaApplet);
-    if (applet) {
-        applet->setParentItem(m_widget);
-        m_applets.append(applet);
-        connect(applet, SIGNAL(geometryChecked()), this, SLOT(checkGeometry()));
-        connect(applet, SIGNAL(destroyed(QObject*)), this, SLOT(appletRemoved(QObject*)));
-        applet->setFlag(QGraphicsItem::ItemIsMovable, false);
-        applet->setBackgroundHints(Plasma::Applet::NoBackground);
-        applet->setObjectName(name);
-        connect(applet, SIGNAL(configNeedsSaving()), this, SIGNAL(configNeedsSaving()));
-        m_layout->addItem(applet);
-        applet->init();
-
-        KConfigGroup cg = config();
-        saveState(cg);
-        emit configNeedsSaving();
-    } else if (plasmaApplet) {
-        delete plasmaApplet;
-    }
-
-    return applet;
-}
-
-void SystemMonitor::removeApplet(const QString &name)
-{
-    foreach (SM::Applet *applet, m_applets) {
-        if (applet->objectName() == name) {
-            applet->destroy();
-        }
-    }
-}
-
-void SystemMonitor::appletRemoved(QObject *object)
-{
-    SM::Applet *applet = static_cast<SM::Applet*>(object);
-
-    foreach (SM::Applet *a, m_applets) {
-        if (a == applet) {
-            m_layout->removeItem(applet);
-            m_applets.removeAll(applet);
-            checkGeometry();
-
-            KConfigGroup cg = config();
-            saveState(cg);
-            emit configNeedsSaving();
-        }
-    }
-
-    // sanity check the buttons
-    QSet<QString> running;
-    foreach (SM::Applet *a, m_applets) {
-        running << a->objectName();
-    }
-
-    foreach (MonitorButton* button, m_monitorButtons) {
-        if (!running.contains(button->objectName())) {
-            kDebug() << "unchecking" << button->objectName();
-            button->setChecked(false);
-        }
-    }
-}
-
-void SystemMonitor::checkGeometry()
-{
-    qreal minHeight = m_buttons->minimumHeight();
-    //kDebug() << minHeight;
-
-    foreach (SM::Applet *applet, m_applets) {
-        //kDebug() << applet->minSize() << applet->minimumSize()
-        //         << applet->metaObject()->className() << applet->size() - applet->contentsRect().size();
-        minHeight += applet->preferredSize().height() + m_layout->spacing();
-    }
-
-
-    update();
-    /*
-    kDebug() << m_widget->size().height() << m_layout->geometry().height();
-    foreach (SM::Applet *applet, m_applets) {
-        kDebug() << applet->metaObject()->className() << applet->size().height();
-    }
-    for (int i = 0; i < m_layout->count(); ++i) {
-        kDebug() << m_layout->itemAt(i)->geometry().top() << m_layout->itemAt(i)->geometry().height();
-    }
-    */
 }
 
 QGraphicsWidget *SystemMonitor::graphicsWidget()
 {
-    return m_widget;
+    return m_systemmonitorwidget;
 }
 
-void SystemMonitor::constraintsEvent(Plasma::Constraints constraints)
-{
-    Plasma::Constraints passOn = Plasma::NoConstraint;
-
-    if (constraints & Plasma::ImmutableConstraint) {
-        foreach (MonitorButton* button, m_monitorButtons) {
-            button->setEnabled(immutability() == Plasma::Mutable);
-        }
-
-        passOn |= Plasma::ImmutableConstraint;
-    }
-
-    if (constraints & Plasma::StartupCompletedConstraint) {
-        passOn |= Plasma::StartupCompletedConstraint;
-    }
-
-    if (passOn != Plasma::NoConstraint) {
-        foreach (Plasma::Applet *applet, m_applets) {
-            applet->updateConstraints(passOn);
-            if (passOn & Plasma::StartupCompletedConstraint) {
-                applet->flushPendingConstraintsEvents();
-            }
-        }
-    }
-
-    PopupApplet::constraintsEvent(constraints);
-}
+K_EXPORT_PLASMA_APPLET(system-monitor_applet, SystemMonitor)
 
 #include "moc_system-monitor.cpp"
+#include "system-monitor.moc"
--- plasma/applets/system-monitor/system-monitor.h
+++ plasma/applets/system-monitor/system-monitor.h
@@ -1,67 +1,42 @@
 /*
- *   Copyright (C) 2007 Petri Damsten <damu@iki.fi>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the
- *   Free Software Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+    This file is part of the KDE project
+    Copyright (C) 2024 Ivailo Monev <xakepa10@gmail.com>
 
-#ifndef SYSTEM_MONITOR_HEADER
-#define SYSTEM_MONITOR_HEADER
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2, as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef SYSTEM_MONITOR_H
+#define SYSTEM_MONITOR_H
 
 #include <Plasma/PopupApplet>
-#include <Plasma/DataEngine>
 
-namespace SM {
-    class Applet;
-}
-class MonitorButton;
-
-#include <QGraphicsLinearLayout>
+class SystemMonitorWidget;
 
 class SystemMonitor : public Plasma::PopupApplet
 {
     Q_OBJECT
-    public:
-        SystemMonitor(QObject *parent, const QVariantList &args);
-        virtual ~SystemMonitor();
+public:
+    SystemMonitor(QObject *parent, const QVariantList &args);
+    ~SystemMonitor();
 
-        void init();
-        virtual QGraphicsWidget *graphicsWidget();
-        virtual void constraintsEvent(Plasma::Constraints constraints);
+    void init();
+    QGraphicsWidget *graphicsWidget();
 
-    public slots:
-        void checkGeometry();
-
-    protected slots:
-        void toggled(bool toggled);
-        void appletRemoved(QObject *object);
-        void configChanged();
-
-    protected:
-        SM::Applet *addApplet(const QString &name);
-        void removeApplet(const QString &name);
-        void saveState(KConfigGroup &group) const;
-        void createConfigurationInterface(KConfigDialog *parent);
-
-    private:
-        QGraphicsLinearLayout *m_layout;
-        QGraphicsLinearLayout *m_buttons;
-        QList<SM::Applet*> m_applets;
-        QList<MonitorButton*> m_monitorButtons;
-        QGraphicsWidget *m_widget;
+private:
+    friend SystemMonitorWidget;
+    SystemMonitorWidget *m_systemmonitorwidget;
 };
 
-K_EXPORT_PLASMA_APPLET(system-monitor_applet, SystemMonitor)
-
-#endif
+#endif // SYSTEM_MONITOR_H
